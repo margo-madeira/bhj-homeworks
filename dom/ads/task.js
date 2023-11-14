743880267f975d173e372841ebdf3321fd712228
margo-madeira <ligastaeva@mail.ru>
2023-11-14 10:33:15 +0300

    change

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,22 +1,17 @@
 const baner = document.querySelectorAll(".rotator__case");
 const baners = Array.from(baner);
 
-function rotator() {
-     
-    for(let i = 0; i < baners.length; i++) {
-        setTimeout(() => {
-
-            baners[i].classList.add("rotator__case_active");  
-            for(let j = 0; j < baners.length; j++) {
-                if(j < i) {
-                    baners[j].classList.remove("rotator__case_active");
-                }
-            }
-        }, 1000 + (i * 1000))
+let indOfBaners = 0;
+setInterval(() => {
+    
+    baners[indOfBaners].classList.remove("rotator__case_active");
+    indOfBaners += 1;
+    
+    baners[indOfBaners].classList.add("rotator__case_active");
+    
+    if(indOfBaners === baners.length - 1) {
+        baners[indOfBaners].classList.remove("rotator__case_active");
+        indOfBaners = 0;
+        baners[indOfBaners].classList.add("rotator__case_active");
     }
-}
-
-rotator();
-
-
-
+}, 1000)
